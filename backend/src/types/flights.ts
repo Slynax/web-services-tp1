f3601325170f25ef7f0d888f408660282aaefda9
Crackvignoule <kiki.kalagan@gmail.com>
2025-06-16 15:12:13 +0200

    revert types

--- backend/src/types/flights.ts
+++ backend/src/types/flights.ts
@@ -1,1 +1,11 @@
-export type { Flight } from '@shared/types/Flight';
+export interface Flight {
+    id: number;
+    numero: string;
+    compagnie: string;
+    origine: string;
+    destination: string;
+    heureDepart: string; // ISO format (ex: "2023-10-01T10:00:00Z")
+    heureArrivee: string;
+    prix: number;
+    places: number;
+  }
