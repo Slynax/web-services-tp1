cb050649149c4c6d24d8f2b0d94f1bc658281331
Slynax <remlouplay3@gmail.com>
2025-06-16 15:24:05 +0200

    récup token OK

--- backend/src/routes/api/v1/index.ts
+++ backend/src/routes/api/v1/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import Flights from './flights';
 import Auth from './auth';
+import Callback from './callback';
 
 const router = Router();
 
 router.use('/flights', Flights);
 router.use('/auth', Auth);
+router.use('/callback', Callback);
 
 export default router;
